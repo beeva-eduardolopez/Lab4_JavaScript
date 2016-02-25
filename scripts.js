f4d436d3bf6043c9d02526de80602735b66db900
beeva-eduardolopez <bbvauser@BBVAs-MacBook-Pro.local>
2016-02-25 12:08:12 -0600

    calculate

--- scripts.js
+++ scripts.js
@@ -17,11 +17,13 @@ function checkAge(){
     }
 }
 
+
+
 function add(op1, op2) {
     return op1 + op2;
 
 }
-function substract(op1, op2) {
+function subtract(op1, op2) {
     return op1 - op2;
   
 }
@@ -30,10 +32,36 @@ function multiply(op1, op2) {
 
 }
 function divide(op1, op2) {
+    if(op2==0){
+        alert('no 0 please');
+    }
     return op1 / op2;
 
 }
 
+function calculate(operation){
+    var op1 = document.getElementById('op1').value;
+    var op2 = document.getElementById('op2').value;
+    var result = 0;
+    op1 = parseInt(op1);
+    op2 = parseInt(op2);
+    switch (operation) {
+        case 'add':
+             result = add(op1,op2);
+            break;
+        case 'subtract':
+             result = subtract(op1,op2);
+            break;
+       case 'multiply':
+             result = multiply(op1,op2);
+            break;
+        case 'divide':
+             result = divide(op1,op2);
+            break;
+    }
+    alert('Result ' +result);
+    console.log('The result is ' +result);
+}
 
 function addParagraph(){
     var par = document.createElement('p');
